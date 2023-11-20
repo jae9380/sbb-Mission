d8dab158498be7e85e0afced65aacaee6fb46d53
jae9380 <ljy5314@gmail.com>
2023-11-20 11:21:30 +0900

    setting

---
+++ src/main/java/com/example/sbbmissoin/SbbMissoinApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbbmissoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbMissoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbMissoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbbmissoin/SbbMissoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbbmissoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbMissoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
