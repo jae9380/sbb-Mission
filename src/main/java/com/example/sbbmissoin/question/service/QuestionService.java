0d59326946557570f2a4c7a8bc948fb9e9992ab1
jae9380 <ljy5314@gmail.com>
2023-11-22 16:28:06 +0900

    sec02-chap10

--- src/main/java/com/example/sbbmissoin/question/service/QuestionService.java
+++ src/main/java/com/example/sbbmissoin/question/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.example.sbbmissoin.question.service;
 
 import com.example.sbbmissoin.question.entity.Question;
+import com.example.sbbmissoin.DataNotFoundException;
 import com.example.sbbmissoin.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +17,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question>question = this.questionRepository.findById(id);
+        if (question.isPresent()){
+            return question.get();
+        }else {
+            throw new DataNotFoundException("question is not found");
+        }
     }
 }
